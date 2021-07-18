c05ed7e5de64063d63e9800f6c2eb91ee44f1df2
Alex Marin - FT13 <alexmarinmendez@gmail.com>
2021-07-18 16:23:50 -0500

    Use cors middleware to enable access localhost:3000 front-end app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 // Settings
 app.set('port', process.env.PORT || 4000);
 app.set('json spaces', 2);
+const config = {
+  application: {
+      cors: {
+          server: [
+              {
+                  origin: "localhost:3000", //servidor que deseas que consuma o (*) en caso que sea acceso libre
+                  credentials: true
+              }
+          ]
+      }
+  }
+};
 
 // Middlewares
+app.use(cors(
+    config.application.cors.server
+  ));
 app.use(express.json());
 
 // Routes
